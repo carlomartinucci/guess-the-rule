22eed32c3f0c6e7cb89b7e2d05d2ac2a8930d8dc
Carlo Martinucci <carlo.martinucci@gmail.com>
2018-10-16 00:42:11 +0200

    Updates

---
+++ precache-manifest.c56d658a5e432c51192cdd2866c14bd7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5eb238d8d919ac203645",
+    "url": "/guess-the-rule/static/js/runtime~main.5eb238d8.js"
+  },
+  {
+    "revision": "1c0b5e9fce2846c3539e",
+    "url": "/guess-the-rule/static/js/main.1c0b5e9f.chunk.js"
+  },
+  {
+    "revision": "8183cff596bb5ef5f899",
+    "url": "/guess-the-rule/static/js/1.8183cff5.chunk.js"
+  },
+  {
+    "revision": "1c0b5e9fce2846c3539e",
+    "url": "/guess-the-rule/static/css/main.b5a7435c.chunk.css"
+  },
+  {
+    "revision": "6f265b1351924e759e9b6543a9d927f2",
+    "url": "/guess-the-rule/index.html"
+  }
+];
